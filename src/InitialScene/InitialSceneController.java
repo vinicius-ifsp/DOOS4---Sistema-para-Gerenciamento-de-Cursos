560d4b30f17c59e2363857cb4ecb36463b1031ef
PecoCaballero <peco.almeida.caballero@gmail.com>
2019-10-22 10:14:25 -0300

    chore: Created InitialScreen layout

---
+++ src/InitialScene/InitialSceneController.java
@@ -0,0 +1,4 @@
+package InitialScene;
+
+public class InitialSceneController {
+}
--- src/InitialScene/Main.java
+++ src/InitialScene/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package InitialScene;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("InitialScene.fxml"));
+        primaryStage.setTitle("iCollege");
+        primaryStage.setScene(new Scene(root, 800, 647));
         primaryStage.show();
     }
 
