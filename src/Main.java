568870ccd9a683a2f4bb2a3f28f7db6cb8746768
PecoCaballero <peco.almeida.caballero@gmail.com>
2019-10-24 08:33:15 -0300

    Fixed Conflicts

--- src/Main.java
+++ src/Main.java
@@ -8,12 +8,12 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("Views/InitialScene.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../Views/SubjectsImport.fxml"));
         primaryStage.setTitle("iCollege");
-        primaryStage.setScene(new Scene(root, 800, 647));
+        primaryStage.setScene(new Scene(root));
+        primaryStage.resizableProperty().setValue(Boolean.FALSE);
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
