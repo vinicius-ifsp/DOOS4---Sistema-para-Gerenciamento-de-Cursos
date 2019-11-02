68f22e3892a7650501a3dae96e8bdd820832d498
Vinicius Silva <vinicius.luiz19@gmail.com>
2019-11-02 15:02:28 +0000

    Create register to course and discipline

--- src/controllers/InitialScreenModalController.java
+++ src/controllers/InitialScreenModalController.java
@@ -2,9 +2,21 @@ package controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import models.Course;
+import views.loaders.MockSingleton;
 
 public class InitialScreenModalController {
+    @FXML
+    private TextField txtName;
+    @FXML
+    private TextField txtPpc;
+    @FXML
+    private TextField txtPeriodQty;
+    @FXML
+    private TextField txtWorkload;
 
     @FXML
     private Button btnClose;
@@ -12,11 +24,32 @@ public class InitialScreenModalController {
     @FXML
     private void close() {
         Stage stage = (Stage) btnClose.getScene().getWindow();
+        System.out.println(MockSingleton.getInstance().toString());
         stage.close();
     }
 
     @FXML
     private void register() {
-        close();
+        if (!isFormValid()) {
+            // TO-DO show msg in invalid fields
+            return;
+        }
+
+        int code = 3;
+        if (!MockSingleton.getInstance().hasCourse(code)) {
+            Course course = new Course(code, Integer.parseInt(txtPeriodQty.getText()), txtName.getText(),
+                    txtPpc.getText(), Double.parseDouble(txtWorkload.getText()), null);
+
+            MockSingleton.getInstance().addCourse(course);
+            System.out.println(course);
+            // TO - DO Msg de Success
+            close();
+        } else {
+            // TO-DO show msg course already registered
+        }
+    }
+
+    private boolean isFormValid() {
+        return true;
     }
 }
--- src/controllers/SubjectsImportController.java
+++ src/controllers/SubjectsImportController.java
@@ -2,20 +2,55 @@ package controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import models.Course;
+import models.Discipline;
+import views.loaders.MockSingleton;
 
 public class SubjectsImportController {
+    @FXML
+    private TextField txtName;
+    @FXML
+    private TextField txtCode;
+    @FXML
+    private TextField txtWorkload;
     @FXML
     private Button btnCancel;
 
     @FXML
     private void close() {
         Stage stage = (Stage) btnCancel.getScene().getWindow();
+        System.out.println(MockSingleton.getInstance().toString());
         stage.close();
     }
 
     @FXML
     private void register() {
-        close();
+        if (!isFormValid()) {
+            // TO-DO show msg in invalid fields
+            return;
+        }
+
+        // TO-DO Change MockSingleton to class dao
+        Course course = MockSingleton.getInstance().getCourse(1);
+        String code = txtCode.getText();
+        if (!course.hasDiscipline(code)) {
+            Discipline discipline = new Discipline(code, txtName.getText(),
+                    Double.parseDouble(txtWorkload.getText()));
+            System.out.println(discipline);
+            System.out.println("Course name: " + course.getName());
+             course.addDiscipline(discipline);
+
+            // TO - DO Msg de Success
+            close();
+        } else {
+            // TO-DO show msg discipline already registered
+        }
+
+    }
+
+    private boolean isFormValid() {
+        return true;
     }
 }
